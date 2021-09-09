bd1a39aa3d9e61b1ada1d5c74fedbb214d2a3b12
Víctor Núñez <vjnunez@gmail.com>
2021-09-09 16:53:33 +0200

    define state_machine.after_transition in project

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,19 +1,22 @@
 module Spree::OrderDecorator
 
+  # por algun motivo no funciona en producción definir en el decorator el
+  # state_machine.after_transition
+
   def self.prepended(base)
     base.include Spree::TransactionRegistrable
 
     base.has_many :transactions, as: :commissionable, class_name: 'Spree::CommissionTransaction', dependent: :restrict_with_error
     base.belongs_to :affiliate, class_name: 'Spree::Affiliate'
 
-    base.state_machine.after_transition to: :complete, do: :create_commission_transaction
+    # base.state_machine.after_transition to: :complete, do: :create_commission_transaction
   end
   
   private
-    def create_commission_transaction
-      register_commission_transaction(affiliate) if affiliate.present?
-    end
+    # def create_commission_transaction
+    #   register_commission_transaction(affiliate) if affiliate.present?
+    # end
+
+  ::Spree::Order.prepend self
 
 end
-
-::Spree::Order.prepend(Spree::OrderDecorator)
